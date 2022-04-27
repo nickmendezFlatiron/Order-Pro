6f2024844857fb9928f19c061da3c60e113fd5a5
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-04-26 21:32:48 -0400

    appended images to SellingPoints container

--- src/components/home-page/SellingPoints.jsx
+++ src/components/home-page/SellingPoints.jsx
@@ -3,19 +3,24 @@ import {Container ,Row , Col} from 'react-bootstrap'
 
 const SellingPoints = () => {
   return (
-    <Container>
+    <Container className="my-5">
       <Row className="text-center">
         <Col sm={4} >
-          <div>
-      
-          </div>
-       
+          <img src="https://via.placeholder.com/150" alt='150'  className='border rounded-circle'/>
+          <br/>
+          <br/>
           <p>Some Text</p>
         </Col>
         <Col sm={4}>
+          <img src="https://via.placeholder.com/150" alt='150'  className='border rounded-circle'/>
+          <br/>
+          <br/>
           <p>Some Text</p>
         </Col>
         <Col sm={4}>
+          <img src="https://via.placeholder.com/150" alt='150'  className='border rounded-circle'/>
+          <br/>
+          <br/>
           <p>Some Text</p>
         </Col>
       </Row>
